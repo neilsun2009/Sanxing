a3acfa7d7752c5b667b5df8f0c52aa047063e422
benwwchen <qq349466656@gmail.com>
2016-12-04 01:38:01 +0800

    finish nearby page layout & change .gitignore
    todo:
    - radar model
    - intents to other pages

---
+++ app/src/main/java/com/note8/sanxing/nearbyClass.java
@@ -0,0 +1,16 @@
+package com.note8.sanxing;
+
+/**
+ * Created by BenWwChen on 2016/12/4.
+ */
+
+public class NearbyClass {
+    public int avatarId;
+    public String userName;
+    public String question;
+    public NearbyClass(int _avatarId, String _userName, String _question) {
+        avatarId = _avatarId;
+        userName = _userName;
+        question = _question;
+    }
+}
